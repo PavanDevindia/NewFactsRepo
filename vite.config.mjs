697662434069b84a7705fd94cb633180b9928d8e
PavanDevindia <pavan.kp247@gmail.com>
2025-02-27 17:11:30 +0530

    Add files via upload

---
+++ vite.config.mjs
@@ -0,0 +1,43 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import vuetify from 'vite-plugin-vuetify'
+import Components from 'unplugin-vue-components/vite'
+import ViteFonts from 'unplugin-fonts/vite'
+import { fileURLToPath, URL } from 'node:url'
+
+export default defineConfig({
+  base: '/nextgen/newsletter-tools/facts/',
+  plugins: [
+    vue(),
+    vuetify(),
+    Components(),
+    ViteFonts({
+      google: {
+        families: [
+          {
+            name: 'Roboto',
+            styles: 'wght@100;300;400;500;700;900',
+          },
+        ],
+      },
+    }),
+  ],
+  define: { 'process.env': {} },
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+    extensions: [
+      '.js',
+      '.json',
+      '.jsx',
+      '.mjs',
+      '.ts',
+      '.tsx',
+      '.vue',
+    ],
+  },
+  server: {
+    port: 3000,
+  },
+})
